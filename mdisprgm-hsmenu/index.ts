3b47ff801e996df5e0dd15500d8c9f2cd6c5afa0
연두 <108284815+yeondu1062@users.noreply.github.com>
2023-08-03 18:03:34 +0900

    Delete mdisprgm-hsmenu directory

--- mdisprgm-hsmenu/index.ts
+++
@@ -1,6 +0,0 @@
-import { HSChest } from "./blocks/chest";
-import { HSDoubleChest } from "./blocks/double_chest";
-import { HSBlock } from "./hsblock";
-import { HSMenu } from "./hsmenu";
-
-export { HSChest, HSDoubleChest, HSBlock, HSMenu };
